98aa30e1eb0b23ee6058e673c58a18882636a20a
Dhawan Gupta <dhawangupta@yahoo.in>
2015-12-15 22:03:48 +0530

    TODO for ravin

--- demo/src/main/java/com/placediscovery/Constants.java
+++ demo/src/main/java/com/placediscovery/Constants.java
@@ -6,8 +6,17 @@ package com.placediscovery;
 public final  class Constants
 {
     public static final String Varanasi="Varanasi";
+
+    //These are going to be used for the initial point in the map when adding a place
     public static double varanasiLat=25.480694;
     public static double varanasiLon=83.6;
+
+    //TODO: add corrected lat and long and for other cities also
+    public static double bangaloreLat=25.480694;
+    public static double bangaloreLon=83.6;
+    public static double kolkataLat=25.480694;
+    public static double kolkataLon=83.6;
+
     public static final String Latitutude="LATITUDE";
     public static final String Longitude="LONGITUDE";
 }
--- demo/src/main/java/com/placediscovery/ui/activity/addingPlace/AddPlaceSelectCity.java
+++ demo/src/main/java/com/placediscovery/ui/activity/addingPlace/AddPlaceSelectCity.java
@@ -1,29 +1,40 @@
 package com.placediscovery.ui.activity.addingPlace;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.placediscovery.R;
 
-import java.util.ArrayList;
 
 /**
  * Created by Dhawan Gupta on 15-12-2015.
  */
+//TODO:Sarkar design it as you wish
+
 public class AddPlaceSelectCity extends ListActivity {
 
-    ArrayList<String> listItems = new ArrayList<>();
+    private static String[] cityArray = {"Kolkata", "Mumbai", "New Delhi", "Chennai", "Bangalore", "Varanasi", "Jaipur"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place_select_city);
 
-
         setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, listItems));
+                android.R.layout.simple_list_item_1, cityArray));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String selectedCity = (String) getListAdapter().getItem(position);
 
 
     }
-    }
+}
