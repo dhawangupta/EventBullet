3653df363fab50f45d82659bfe33f7f9a2fca08c
Dhawan Gupta <dhawangupta@yahoo.in>
2015-12-15 22:12:56 +0530

    changed style for lat long in constants

--- demo/src/main/java/com/placediscovery/Constants.java
+++ demo/src/main/java/com/placediscovery/Constants.java
@@ -1,5 +1,7 @@
 package com.placediscovery;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by Dhawan Gupta on 08-12-2015.
  */
@@ -8,14 +10,14 @@ public final  class Constants
     public static final String Varanasi="Varanasi";
 
     //These are going to be used for the initial point in the map when adding a place
-    public static double varanasiLat=25.480694;
-    public static double varanasiLon=83.6;
+    // first one is lattitude and second one is longitude
+
+    LatLng varanasiLatLng = new LatLng(25.480694,83.6);
 
     //TODO: add corrected lat and long and for other cities also
-    public static double bangaloreLat=25.480694;
-    public static double bangaloreLon=83.6;
-    public static double kolkataLat=25.480694;
-    public static double kolkataLon=83.6;
+    LatLng bangaloreLatLng = new LatLng(25.480694,83.6);
+    LatLng kolkataLatLng = new LatLng(25.480694,83.6);
+    LatLng jaipurLatLng = new LatLng(25.480694,83.6);
 
     public static final String Latitutude="LATITUDE";
     public static final String Longitude="LONGITUDE";
