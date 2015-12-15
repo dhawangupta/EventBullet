da058fb32b19dea59b3b2cbd04f0271944b4c92f
ravinsingh16 <ravin.singh.eee12@itbhu.ac.in>
2015-12-16 01:51:53 +0530

    Update Constants.java

--- demo/src/main/java/com/placediscovery/Constants.java
+++ demo/src/main/java/com/placediscovery/Constants.java
@@ -12,12 +12,12 @@ public final  class Constants
     //These are going to be used for the initial point in the map when adding a place
     // first one is lattitude and second one is longitude
 
-    public static LatLng varanasiLatLng = new LatLng(25.480694,83.6);
+    public static LatLng varanasiLatLng = new LatLng(25.28,82.96);
 
     //TODO: add corrected lat and long and for other cities also
-    public static LatLng bangaloreLatLng = new LatLng(25.480694,83.6);
-    public static LatLng kolkataLatLng = new LatLng(25.480694,83.6);
-    public static LatLng jaipurLatLng = new LatLng(25.480694,83.6);
+    public static LatLng bangaloreLatLng = new LatLng(12.96,77.56);
+    public static LatLng kolkataLatLng = new LatLng(22.56,88.36);
+    public static LatLng jaipurLatLng = new LatLng(26.9,75.8);
 
     public static final String Latitutude="LATITUDE";
     public static final String Longitude="LONGITUDE";
