5e25ee467b5552eb3477cc467a1c490ebf780068
Dhawan Gupta <dhawangupta@yahoo.in>
2015-12-27 10:54:54 +0530

    Revert "Removed Facebook and migrated to Mapbox"
    This reverts commit 2154f3eeb34f320272efe6ea91ff5744192aac23.

---
+++ demo/src/main/java/com/placediscovery/Constants.java
@@ -0,0 +1,31 @@
+package com.placediscovery;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Dhawan Gupta on 08-12-2015.
+ */
+public final  class Constants
+{
+    public static final String Varanasi="Varanasi";
+    public static final String Bangalore="Bangalore";
+    public static final String Jaipur="Jaipur";
+    public static final String Kolkata="Kolkata";
+    public static final String Mumbai="Mumbai";
+    public static final String NewDelhi="NewDelhi";
+    public static final String Chennai="Chennai";
+
+    //These are going to be used for the initial point in the map when adding a place
+    // first one is lattitude and second one is longitude
+
+    public static LatLng varanasiLatLng = new LatLng(25.28,82.96);
+    public static LatLng bangaloreLatLng = new LatLng(12.96,77.56);
+    public static LatLng kolkataLatLng = new LatLng(22.56,88.36);
+    public static LatLng jaipurLatLng = new LatLng(26.9,75.8);
+    public static LatLng mumbaiLatLng = new LatLng(18.98,72.83);
+    public static LatLng newdelhiLatLng = new LatLng(28.61,77.21);
+    public static LatLng chennaiLatLng = new LatLng(13.08,80.27);
+
+    public static final String Latitutude="LATITUDE";
+    public static final String Longitude="LONGITUDE";
+}
