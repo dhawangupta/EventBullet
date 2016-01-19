5d1386e9b5eddc323815e7f69bb0dc306bdc4ab3
Dhawan Gupta <dhawangupta@yahoo.in>
2016-01-19 19:16:17 +0530

    Ratings also going in users collection
    now ratings given by user also get reflected in user's document in
    mongodb also.  (earlier, it was only in place's document)

--- app/src/main/java/com/placediscovery/ui/activity/ContentActivity.java
+++ app/src/main/java/com/placediscovery/ui/activity/ContentActivity.java
@@ -1,6 +1,7 @@
 package com.placediscovery.ui.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -17,12 +18,17 @@ import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.placediscovery.ImageLoader.ImageLoader;
 import com.placediscovery.MongoLabPlace.Place;
+import com.placediscovery.MongoLabPlace.PlaceQueryBuilder;
 import com.placediscovery.MongoLabPlace.UpdatePlaceAsyncTask;
 import com.placediscovery.MongoLabUser.UpdateUserAsyncTask;
 import com.placediscovery.MongoLabUser.User;
+import com.placediscovery.MongoLabUser.UserQueryBuilder;
 import com.placediscovery.MongoLabUser.UserStatus;
 import com.placediscovery.R;
 
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -32,11 +38,15 @@ public class ContentActivity extends AppCompatActivity implements
     String selectedCity;
     private SliderLayout mDemoSlider;   //this is imageslider used
     private RatingBar ratingBar;
+    User loggedInUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+
+        UserStatus userStatus = new UserStatus();
+        loggedInUser = new User(userStatus);
 
         mDemoSlider = (SliderLayout)findViewById(R.id.contentPageImageSlider);
 
@@ -121,18 +131,23 @@ public class ContentActivity extends AppCompatActivity implements
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
                 //TODO: add for rating by user and also change to suitable UI for Rating
-                //if (UserStatus.isLoginStatus()) {
-                    int newCount = currentCount+1;
-                    double newRating = (currentRating*currentCount+rating)/newCount;
+                if (UserStatus.isLoginStatus()) {
+                    int newCount = currentCount + 1;
+                    double newRating = (currentRating * currentCount + rating) / newCount;
                     selectedPlace.setCount(String.valueOf(newCount));
                     selectedPlace.setAverageRating(String.valueOf(newRating));
 
                     UpdatePlaceAsyncTask tsk = new UpdatePlaceAsyncTask(selectedCity);
                     tsk.execute(selectedPlace);
-                //}
 
-                Toast.makeText(ContentActivity.this, "New Rating: " + rating,
-                        Toast.LENGTH_SHORT).show();
+                    UpdateUserRatingAsyncTask task = new UpdateUserRatingAsyncTask();
+                    task.execute(loggedInUser,selectedPlace.getPlace_id(),Float.toString(rating));
+
+                    Toast.makeText(ContentActivity.this, "New Rating: " + rating,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ContentActivity.this, "Please Login", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -140,8 +155,6 @@ public class ContentActivity extends AppCompatActivity implements
             @Override
             public void onClick(View v) {
                 if (UserStatus.isLoginStatus()) {
-                    UserStatus userStatus = new UserStatus();
-                    User loggedInUser = new User(userStatus);
 
                     /*TODO: place_id or name of place (we have to devise a method to display saved places in SavedPlaces.java
                     * TODO: check for adding same place multiple items
@@ -161,6 +174,43 @@ public class ContentActivity extends AppCompatActivity implements
         });
 
     }
+
+    class UpdateUserRatingAsyncTask extends AsyncTask<Object, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Object... params) {
+            User user = (User) params[0];
+            String place_id = (String) params[1];
+            String rating = (String) params[2];
+
+            try {
+
+                UserQueryBuilder qb = new UserQueryBuilder();
+                URL url = new URL(qb.buildUsersUpdateURL(user.getUser_id()));
+                HttpURLConnection connection = (HttpURLConnection) url
+                        .openConnection();
+                connection.setRequestMethod("PUT");
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Type",
+                        "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+
+                OutputStreamWriter osw = new OutputStreamWriter(
+                        connection.getOutputStream());
+
+                osw.write(qb.addNewRatingbyUser(place_id,rating));
+                osw.flush();
+                osw.close();
+                return connection.getResponseCode() < 205;
+            } catch (Exception e) {
+                e.getMessage();
+                return false;
+            }
+
+        }
+
+    }
+
 
 
     @Override
