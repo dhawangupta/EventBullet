9dbb95b1cd4c00a1afccfc520422a95d1d3cd27a
Zubersk <shaik.mzuber.eee12@itbhu.ac.in>
2016-02-05 21:45:35 +0530

    splash activity added

---
+++ app/src/main/java/com/placediscovery/ui/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.placediscovery.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.placediscovery.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
